a56b02330677e2b94ca90ae902026cdcb48eb451
timurTK30 <tymiramongas@gmail.com>
2024-02-27 20:40:21 +0100

    Bot telegram

--- src/main/java/com/example/myTelegramBot/MyBot.java
+++ src/main/java/com/example/myTelegramBot/MyBot.java
@@ -3,7 +3,6 @@ package com.example.myTelegramBot;
 import com.example.myTelegramBot.config.BotConfig;
 import com.example.myTelegramBot.domain.Product;
 import com.example.myTelegramBot.domain.User;
-import com.example.myTelegramBot.service.UserService;
 import com.example.myTelegramBot.service.serviceImpl.ProductServiceImpl;
 import com.example.myTelegramBot.service.serviceImpl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -14,21 +13,21 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import javax.security.auth.callback.Callback;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 //my telegram bot
 @Component
 @Slf4j
 public class MyBot extends TelegramLongPollingBot {
 
+    List<Product> cart = new ArrayList<>();
     private BotConfig botConfig;
     private UserServiceImpl userServiceimpl;
-
     private ProductServiceImpl productService;
 
 
@@ -39,37 +38,28 @@ public class MyBot extends TelegramLongPollingBot {
         this.productService = productService;
     }
 
-
-
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.getMessage().hasText() && update.hasMessage()){
+        if (update.hasMessage()) {
             String msg = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
 
-            if(msg.startsWith("/reg")){
+            if (msg.startsWith("/reg")) {
                 registerUser(update, msg);
-            }else if (msg.startsWith("/menu")){
+            } else if (msg.startsWith("/menu")) {
                 productMenu(chatId);
-            }else if (msg.startsWith("/start")){
+            } else if (msg.startsWith("/start")) {
                 startMenu(chatId);
-            }else if (msg.startsWith("/buy")){
+            } else if (msg.startsWith("/buy")) {
                 cart(chatId, msg);
-            }else if(msg.startsWith("/hi")){
-                try {
-                    execute(
-                            SendMessage.builder().chatId(chatId).text("Привет").
-                                    replyMarkup(createHiKeyboard()).build()
-                    );
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
-                }
+            } else if (msg.startsWith("/hi")) {
+                sendMassageToUser(chatId, msg, List.of("hi", "bye", "hahah"));
             }
         }
-        if (update.hasCallbackQuery()){
+        if (update.hasCallbackQuery()) {
             CallbackQuery callbackQuery = update.getCallbackQuery();
-            if (callbackQuery.getData().equalsIgnoreCase("hi")){
-                sendMessage(callbackQuery.getFrom().getId(), "ok");
+            if (callbackQuery.getData().equalsIgnoreCase("hi")) {
+                sendMessage(callbackQuery.getMessage().getChatId(), "ok");
             }
         }
     }
@@ -94,43 +84,60 @@ public class MyBot extends TelegramLongPollingBot {
         return keyboardMarkup;
     }
 
-    private void startMenu(Long chatId){
+    private InlineKeyboardMarkup createCustomKeyboard(List<String> buttons) {
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyBoard = new ArrayList<>();
+        for (String s : buttons) {
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText(s);
+            button.setCallbackData(s);
+
+            List<InlineKeyboardButton> row1 = new ArrayList<>();
+            row1.add(button);
+
+            keyBoard.add(row1);
+        }
+
+        keyboardMarkup.setKeyboard(keyBoard);
+
+        return keyboardMarkup;
+    }
+
+    private void startMenu(Long chatId) {
         sendMessage(chatId, "Добро пожаловать в моего первого бота!\n" +
                 "команды: \n" +
                 "/reg - позволает зарегестрироватся\n" +
                 "/menu - меню покупок\n " +
-                "/buy - добавить в корзину");
+                "/buy - добавить в корзину", null);
     }
 
-    List<Product> cart = new ArrayList<>();
-
-    private void cart(Long chatId, String msg){
+    private void cart(Long chatId, String msg) {
         List<Product> productsFromMap = productService.readAll();
         Integer id = Integer.valueOf(msg.split(" ")[1]);
         Product product = productsFromMap.get(id - 1);
         cart.add(product);
         System.out.println(cart);
-        sendMessage(chatId, cart.toString());
+        sendMessage(chatId, cart.toString(), null);
     }
 
-    private void productMenu( Long chatId){
+    private void productMenu(Long chatId) {
         List<Product> products = productService.readAll();
         StringBuilder menuText = new StringBuilder("Выберите товар для покупки\n\n");
         int index = 1;
-        for (Product p: products){
+        for (Product p : products) {
             menuText.append(index).
                     append(". ").append(p.getName()).
                     append(" цена: ").append(p.getPrice()).append("\n");
             index++;
         }
-        sendMessage(chatId, menuText.toString());
+        sendMessage(chatId, menuText.toString(), null);
     }
 
     private void registerUser(Update update, String text) {
         String[] words = text.split(" ");
         String emeil = words[1];
         Long chatId = update.getMessage().getChatId();
-        if(!isUserExist(chatId)){
+        if (!isUserExist(chatId)) {
             String username = update.getMessage().getFrom().getUserName();
             userServiceimpl.save(
                     User.builder()
@@ -139,16 +146,21 @@ public class MyBot extends TelegramLongPollingBot {
                             .email(emeil)
                             .build()
             );
-        }else {
-            sendMessage(chatId, "Вы уже зарегестриривоини");
+        } else {
+            sendMessage(chatId, "Вы уже зарегестриривоини", null);
         }
 
-    }
 
-    public void sendMessage(Long chatId, String msg){
+    private void sendMessage(Long chatId, String msg, List<String> buttons) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
         sendMessage.setText(msg);
+
+        if (buttons != null) {
+            InlineKeyboardMarkup keyboardMarkup = createCustomKeyboard(buttons);
+            sendMessage.setReplyMarkup(keyboardMarkup);
+        }
+
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
@@ -157,11 +169,11 @@ public class MyBot extends TelegramLongPollingBot {
     }
 
 
-    private boolean isUserExist(Long id){
+    private boolean isUserExist(Long id) {
         Optional<User> userById = userServiceimpl.findUserByChatId(id);
-        if(userById.isEmpty()){
+        if (userById.isEmpty()) {
             return false;
-        }else {
+        } else {
             return true;
         }
     }
